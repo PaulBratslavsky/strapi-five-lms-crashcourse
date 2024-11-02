f1024913d2cb8ff46786583c2ca387efd0eef2d4
paul bratslavsky <paulbratslavsky@Mac.localdomain>
2024-11-02 14:35:42 -0500

    update Strapi to recent release

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,16 +1,4 @@
-import type { Struct, Schema } from '@strapi/strapi';
-
-export interface LayoutTopNavigation extends Struct.ComponentSchema {
-  collectionName: 'components_layout_top_navigations';
-  info: {
-    displayName: 'Top Navigation';
-  };
-  attributes: {
-    logoTitle: Schema.Attribute.Component<'elements.link', false>;
-    navItems: Schema.Attribute.Component<'elements.link', true>;
-    cta: Schema.Attribute.Component<'elements.link', false>;
-  };
-}
+import type { Schema, Struct } from '@strapi/strapi';
 
 export interface ElementsLink extends Struct.ComponentSchema {
   collectionName: 'components_elements_links';
@@ -18,18 +6,30 @@ export interface ElementsLink extends Struct.ComponentSchema {
     displayName: 'Link';
   };
   attributes: {
-    label: Schema.Attribute.String;
     href: Schema.Attribute.String;
     isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String;
     type: Schema.Attribute.Enumeration<['LINK', 'PRIMARY', 'SECONDARY']>;
+  };
+}
+
+export interface LayoutTopNavigation extends Struct.ComponentSchema {
+  collectionName: 'components_layout_top_navigations';
+  info: {
+    displayName: 'Top Navigation';
+  };
+  attributes: {
+    cta: Schema.Attribute.Component<'elements.link', false>;
+    logoTitle: Schema.Attribute.Component<'elements.link', false>;
+    navItems: Schema.Attribute.Component<'elements.link', true>;
   };
 }
 
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'layout.top-navigation': LayoutTopNavigation;
       'elements.link': ElementsLink;
+      'layout.top-navigation': LayoutTopNavigation;
     }
   }
 }
